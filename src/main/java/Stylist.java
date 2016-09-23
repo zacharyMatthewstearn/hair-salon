8bff9dace93059192c78262721aa439479f01700
Zachary Matthewstearn <zammo911@gmail.com>
2016-09-23 13:49:50 -0700

    add and test Stylist class

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,15 +1,82 @@
-public class Stylist {
-  private int id;
-  private String description;
+import java.util.List;
+import java.util.ArrayList;
+import org.sql2o.*;
 
-  public Stylist(String _description) {
-    description = _description;
+public class Stylist {
+  // Member Variables
+  private int id;
+  private String name;
+
+  // Constructor
+  public Stylist(String _name) {
+    name = _name;
   }
 
+  // Static Methods
+  public static List<Stylist> getAll() {
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery("SELECT * FROM stylists")
+        .executeAndFetch(Stylist.class);
+    }
+  }
+  public static Stylist getById(int _id) {
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery("SELECT * FROM stylists WHERE id = :id")
+        .addParameter("id", _id)
+        .executeAndFetchFirst(Stylist.class);
+    }
+  }
+
+  // Overrides
+  @Override
+  public boolean equals(Object _otherStylist) {
+    if(!(_otherStylist instanceof Stylist))
+      return false;
+    Stylist newStylist = (Stylist) _otherStylist;
+      return id == newStylist.getId() &&
+             name.equals(newStylist.getName());
+  }
+
+  // Getters
   public int getId() {
     return id;
   }
   public String getName() {
-    return description;
+    return name;
+  }
+
+  // Setters
+  public void setName(String _newName) {
+    name = _newName;
+    try(Connection con = DB.sql2o.open()) {
+      con.createQuery("UPDATE stylists SET name = :name WHERE id = :id", true)
+      .addParameter("name", name)
+      .addParameter("id", id)
+      .executeUpdate();
+    }
+  }
+
+  // Other Non-Static Methods
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      this.id = (int) con.createQuery("INSERT INTO stylists (name) VALUES (:name)", true)
+      .addParameter("name", name)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      con.createQuery("DELETE FROM stylists WHERE id = :id")
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+  public List<Client> getClients() {
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery("SELECT * FROM clients WHERE stylistId = :id")
+        .addParameter("id", id)
+        .executeAndFetch(Client.class);
+    }
   }
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -5,8 +5,112 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class StylistTest {
-
+  // Rules
   @Rule
   public DatabaseRule databaseRule = new DatabaseRule();
+
+  // Tests
+  //// save
+  @Test
+  public void save_savesStylistToDatabase_true() {
+    Stylist testStylist = new Stylist("Taako");
+    testStylist.save();
+    assertTrue(testStylist.getId() > 0);
+  }
+
+  //// equals
+  @Test
+  public void equals_returnsFalseWhenUnequal_false() {
+    Stylist testStylist1 = new Stylist("Magnus");
+    testStylist1.save();
+    Stylist testStylist2 = new Stylist("Merle");
+    testStylist2.save();
+    assertTrue(!testStylist1.equals(testStylist2));
+  }
+  @Test
+  public void equals_returnsTrueWhenEqual_true() {
+    Stylist testStylist1 = new Stylist("Taako");
+    testStylist1.save();
+    Stylist testStylist2 = testStylist1;
+    testStylist2.save();
+    assertTrue(testStylist1.equals(testStylist2));
+  }
+
+  //// getById
+  @Test
+  public void getById_returnsNullIfStylistNotInDB_null() {
+    Stylist testStylist = new Stylist("Taako");
+    assertEquals(null, Stylist.getById(testStylist.getId()));
+  }
+  @Test
+  public void getById_fetchesCorrectStylistFromDB_Stylist() {
+    Stylist testStylist = new Stylist("Taako");
+    testStylist.save();
+    assertTrue(Stylist.getById(testStylist.getId()).equals(testStylist));
+  }
+
+  //// getAll
+  @Test
+  public void getAll_returnsEmptyListIfNoStylistsInDB_ListStylist() {
+    assertTrue(Stylist.getAll().size() == 0);
+  }
+  @Test
+  public void getAll_fetchesAllStylistsFromDB_ListStylist() {
+    Stylist testStylist1 = new Stylist("Merle");
+    testStylist1.save();
+    Stylist testStylist2 = new Stylist("Magnus");
+    testStylist2.save();
+    assertTrue(Stylist.getAll().get(1).equals(testStylist2));
+  }
+
+  //// delete
+  @Test
+  public void delete_doesNotCrashIfStylistIsNotInDB_null() {
+    Stylist testStylist = new Stylist("Taako");
+    testStylist.delete();
+    assertEquals(null, Stylist.getById(testStylist.getId()));
+  }
+  @Test
+  public void delete_removesStylistFromDatabase_null() {
+    Stylist testStylist = new Stylist("Taako");
+    testStylist.save();
+    testStylist.delete();
+    assertEquals(null, Stylist.getById(testStylist.getId()));
+  }
+
+  //// setName
+  @Test
+  public void setName_doesNotCrashIfStylistIsNotInDB_null() {
+    Stylist testStylist = new Stylist("Taako");
+    testStylist.setName("Barry Bluejeans");
+    assertEquals(null, Stylist.getById(1));
+  }
+  @Test
+  public void setName_changesNameAppropriately_String() {
+    Stylist testStylist = new Stylist("Taako");
+    testStylist.save();
+    testStylist.setName("Barry Bluejeans");
+    assertTrue("DB", Stylist.getById(testStylist.getId()).getName().equals("Barry Bluejeans"));
+    assertTrue("Member variable", testStylist.getName().equals("Barry Bluejeans"));
+  }
+
+  //// getClients
+  @Test
+  public void getClients_returnsEmtpyListIfNoStylistsInDB_ListStylist() {
+    Stylist testStylist = new Stylist("Taako");
+    testStylist.save();
+    assertTrue(testStylist.getClients().size() == 0);
+  }
+  @Test
+  public void getClients_returnsAppropriateClients_ListStylist() {
+    Stylist testStylist = new Stylist("Taako");
+    testStylist.save();
+    Client testClient1 = new Client(testStylist.getId(), "Merle");
+    testClient1.save();
+    Client testClient2 = new Client(testStylist.getId(), "Magnus");
+    testClient2.save();
+    assertTrue("Merle", testStylist.getClients().get(0).equals(testClient1));
+    assertTrue("Magnus", testStylist.getClients().get(1).equals(testClient2));
+  }
 
 }
